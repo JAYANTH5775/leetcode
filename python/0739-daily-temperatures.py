024b0136ae47c1ead17ec49dc148d0eb3c628036
JAYANTH H S <102643623+JAYANTH5775@users.noreply.github.com>
2023-03-19 11:36:52 +0530

    changed the order off the element in the tuple
    changed the order of the element order( index , vaall )  in which its a pushed s o that its not confusing while interchanging and  adding value to the result array

--- python/0739-daily-temperatures.py
+++ python/0739-daily-temperatures.py
@@ -4,8 +4,8 @@ class Solution:
         stack = []  # pair: [temp, index]
 
         for i, t in enumerate(temperatures):
-            while stack and t > stack[-1][0]:
-                stackT, stackInd = stack.pop()
+            while stack and t > stack[-1][1]:
+                stackInd,stackT = stack.pop()
                 res[stackInd] = i - stackInd
-            stack.append((t, i))
+            stack.append((i,t))
         return res
